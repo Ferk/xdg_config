f21a268d684b030300a4f346503208c499768060
Ferk <ferkiwi@gmail.com>
2012-01-16 10:33:03 +0100

    Reallocated the files to be linked to XDG_DATA_HOME to DATA/ and altered the symlinking script to link them too.

---
+++ symlink.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+[ -z $XDG_CONFIG_HOME ] && XDG_CONFIG_HOME="$HOME/.cache"
+
+echo -e "\e[36m** Creating symlinks to HOME...\e[0m"
+ln -sv $@ ${XDG_CONFIG_HOME}/HOME/.??* $HOME
+
+echo -e "\e[36sm** Creating symlinks to XDG_DATA_HOME...\e[0m"
+ln -sv $@ ${XDG_CONFIG_HOME}/DATA/* ${XDG_DATA_HOME:-"$HOME/.local/share"}
+
+
