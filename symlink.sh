f0a2e9cd9c2e693be4fc2c60c8c315b46bb8b7a2
Ferk <ferkiwi@gmail.com>
2012-01-18 02:56:58 +0100

    Removed some garbage character.

--- symlink.sh
+++ symlink.sh
@@ -5,7 +5,7 @@
 echo -e "\e[36m** Creating symlinks to HOME...\e[0m"
 ln -sv $@ ${XDG_CONFIG_HOME}/HOME/.??* $HOME
 
-echo -e "\e[36sm** Creating symlinks to XDG_DATA_HOME...\e[0m"
+echo -e "\e[36m** Creating symlinks to XDG_DATA_HOME...\e[0m"
 ln -sv $@ ${XDG_CONFIG_HOME}/DATA/* ${XDG_DATA_HOME:-"$HOME/.local/share"}
 
 
