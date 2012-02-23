b787f162c495f1e4f1a57ae9322eff4d9299791d
Ferk <ferkiwi@gmail.com>
2012-02-23 22:36:52 +0100

    Replaced some strings for colors with the appropiate color code.

--- HOME/.emacs.d/themes/darkclean-theme.el
+++ HOME/.emacs.d/themes/darkclean-theme.el
@@ -16,13 +16,18 @@
 ;; GNU General Public License for more details.
 ;; <http://www.gnu.org/licenses/>.
 
-;;;
-
+;;; Commentary:
+;;
 ;; Simple theme with dark background colors. It has faces for hl-line,
 ;; whitespace and some other modes. Best seen at 16bit color depth or
 ;; higher (some background color transitions are tenuous and might be
 ;; too strong on low color depths). Created by Fernando Carmona Varo
-
+;;
+;;; Installation:
+;;
+;;   Drop the theme in the `custom-theme-load-path' (typically ~/.emacs.d/themes)
+;;
+;;; Code
 (deftheme darkclean
   "Simple theme that makes use of dark background colors for some special modes (hl-line, whitespace, flymake). Best seen at 16-bit color depth or higher.")
 
@@ -32,12 +37,12 @@
  '(bold ((t (:weight bold :bold t))))
  '(bold-italic ((t (:weight bold :slant italic :bold t :italic t))))
  '(mode-line ((t (:box (:line-width -1 :style released-button)
-                                           :foreground "black" :background "lightgray"))))
+                                           :foreground "#000" :background "#AAA"))))
  '(mode-line-inactive ((t (:box (:line-width -1 :style pressed-button)
                                                   :foreground "#EEE" :background "#444"))))
  '(region ((t (:background "#004"))))
  '(show-paren-match ((t (:background "#330"))))
- '(show-paren-mismatch ((t (:background "red"))))
+ '(show-paren-mismatch ((t (:background "#F00"))))
  '(font-lock-builtin-face ((t (:foreground "#ABE"))))
  '(font-lock-preprocessor-face ((t (:weight extra-bold :inherit (font-lock-builtin-face)))))
  '(font-lock-comment-face ((t (:foreground "#B9B"))))
@@ -58,14 +63,14 @@
  '(lazy-highlight ((t (:background "#640"))))
  '(hl-line ((t (:background "#000"))))
  '(trailing-whitespace ((t (:background "#100"))))
- '(whitespace-tab ((((class color) (background dark)) (:background "grey5" :foreground "grey10"))))
+ '(whitespace-tab ((((class color) (background dark)) (:background "#111" :foreground "#222"))))
  '(whitespace-indentation ((t (:background "#111"))))
  '(whitespace-newline ((t (:foreground "#012" :weight normal))))
  '(whitespace-space-before-tab ((t (:background "#210"))))
  '(whitespace-space-after-tab ((t (:background "#210"))))
  '(whitespace-trailing ((t (:background "#100"))))
- '(flymake-warnline ((((class color) (background dark)) (:underline "blue" :background "#004"))))
- '(flymake-errline ((((class color) (background dark)) (:underline "red" :background "#400"))))
+ '(flymake-warnline ((((class color) (background dark)) (:underline "#00F" :background "#004"))))
+ '(flymake-errline ((((class color) (background dark)) (:underline "#F00" :background "#400"))))
  '(makefile-makepp-perl ((t (:background "#005"))))
  '(makefile-space ((t (:background "#301"))))
  )
@@ -79,3 +84,9 @@
 
 
 (provide-theme 'darkclean)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
+
+;;; darkclean-theme.el ends here.
