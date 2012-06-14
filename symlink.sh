5aaccacc1ba5f9c225edf762969461d684aaed83
Fernando C.V <ferkiwi@gmail.com>
2012-06-14 23:13:16 +0200

    Fixed mistake in the paths

--- symlink.sh
+++ symlink.sh
@@ -6,12 +6,12 @@ export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 echo -e "\e[36m** Creating symlinks to HOME...\e[0m"
 for f in $(ls -A "${XDG_CONFIG_HOME}/HOME")
 do 
-    ln -sv $@ "$f" "$HOME"
+    ln -sv $@ "${XDG_CONFIG_HOME}/HOME/$f" "$HOME"
 done
 
 echo -e "\e[36m** Creating symlinks to XDG_DATA_HOME...\e[0m"
 for f in $(ls -A "${XDG_CONFIG_HOME}/DATA")
 do 
-    ln -sv $@ "$f" "${XDG_DATA_HOME:-$HOME/.local/share}"
+    ln -sv $@ "${XDG_CONFIG_HOME}/DATA/$f" "${XDG_DATA_HOME:-$HOME/.local/share}"
 done
 
