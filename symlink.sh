8cb560842f2765643ef219eca686756441258667
Fernando C.V <ferkiwi@gmail.com>
2013-01-27 19:05:59 +0100

    Check that the data folder exists (and create it if not)

--- symlink.sh
+++ symlink.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 
 
 echo -e "\e[36m** Creating symlinks to HOME...\e[0m"
 for f in $(ls -A "${XDG_CONFIG_HOME}/HOME")
-do 
+do
     ln -sv $@ "${XDG_CONFIG_HOME}/HOME/$f" "$HOME"
 done
 
 echo -e "\e[36m** Creating symlinks to XDG_DATA_HOME...\e[0m"
+mkdir -p "$XDG_DATA_HOME"
 for f in $(ls -A "${XDG_CONFIG_HOME}/DATA")
-do 
-    ln -sv $@ "${XDG_CONFIG_HOME}/DATA/$f" "${XDG_DATA_HOME:-$HOME/.local/share}"
+do
+    ln -sv $@ "${XDG_CONFIG_HOME}/DATA/$f" "$XDG_DATA_HOME"
 done
 
