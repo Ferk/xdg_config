dfc2a58b3d5181289c158575176de6e5a1a67332
Fernando C.V <ferkiwi@gmail.com>
2013-09-17 16:21:34 +0200

    Improved mk

--- sh/openwrt.sh
+++ sh/openwrt.sh
@@ -8,7 +8,18 @@ then
     alias make="make -j$((1+$(cat /proc/cpuinfo | grep -c processor)))"
 fi
 
-alias mk='make V=s 2>&1 | tee make.log | grep -ie "[^-\w]error" -e "^make"'
+mk() {
+    {
+	if make V=s "$@" 2>&1
+	then
+	    echo "Finished with successful error code ($?)!"
+	else
+	    echo "Returned error code: $? ...see make.log for full log"
+	fi
+    } | \
+	tee make.log | grep -ie "[^-_\w]error" -e "^make.*: Entering directory"
+    echo -e "\a"
+}
 
 export PATH=$PATH:./scripts:./scripts/flashing
 
