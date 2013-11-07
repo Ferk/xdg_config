caa3aeb2e695aa9cc037fbf2d66eeb46e957cab8
Fernando C.V <ferkiwi@gmail.com>
2013-11-07 09:58:15 +0100

    Added openwrt linux-rebuild alias

--- sh/openwrt.sh
+++ sh/openwrt.sh
@@ -1,5 +1,23 @@
 
 
+# export OPENWRT=$HOME/sama5/heatsvn/openwrt/
+# export STAGING_DIR=$OPENWRT/staging_dir/
+# export PATH=$STAGING_DIR/host/bin:$PATH:$STAGING_DIR/toolchain-arm_v7-a_gcc-4.6-linaro_uClibc-0.9.33.2_eabi/bin
+
+# export CROSS_OPTS='HOSTCFLAGS="-O2 -I/SAMA53EK/new/staging_dir/host/include -Wall -Wmissing-prototypes -Wstrict-prototypes" CROSS_COMPILE="arm-openwrt-linux-uclibcgnueabi-" ARCH="arm"'
+
+
+
+# make -C /SAMA53EK/new/build_dir/target-arm_v7-a_uClibc-0.9.33.2_eabi/linux-at91sama5/linux-3.10 HOSTCFLAGS="-O2 -I/SAMA53EK/new/staging_dir/host/include -Wall -Wmissing-prototypes -Wstrict-prototypes" CROSS_COMPILE="arm-openwrt-linux-uclibcgnueabi-" ARCH="arm" KBUILD_HAVE_NLS=no CONFIG_SHELL="/usr/bin/bash" V='' CC="arm-openwrt-linux-uclibcgnueabi-gcc" INSTALL_HDR_PATH=/SAMA53EK/new/build_dir/target-arm_v7-a_uClibc-0.9.33.2_eabi/linux-at91sama5/linux-3.10/user_headers headers_install
+
+# make -C /SAMA53EK/new/build_dir/target-arm_v7-a_uClibc-0.9.33.2_eabi/linux-at91sama5/linux-3.10 HOSTCFLAGS="-O2 -I/SAMA53EK/new/staging_dir/host/include -Wall -Wmissing-prototypes -Wstrict-prototypes" CROSS_COMPILE="arm-openwrt-linux-uclibcgnueabi-" ARCH="arm" KBUILD_HAVE_NLS=no CONFIG_SHELL="/usr/bin/bash" V='' CC="arm-openwrt-linux-uclibcgnueabi-gcc" modules
+
+
+# make -C /SAMA53EK/new/build_dir/target-arm_v7-a_uClibc-0.9.33.2_eabi/linux-at91sama5/linux-3.10 HOSTCFLAGS="-O2 -I/SAMA53EK/new/staging_dir/host/include -Wall -Wmissing-prototypes -Wstrict-prototypes" CROSS_COMPILE="arm-openwrt-linux-uclibcgnueabi-" ARCH="arm" KBUILD_HAVE_NLS=no CONFIG_SHELL="/usr/bin/bash" V='' CC="arm-openwrt-linux-uclibcgnueabi-gcc" 
+
+# make -C /SAMA53EK/new/build_dir/target-arm_v7-a_uClibc-0.9.33.2_eabi/linux-at91sama5/linux-3.10 HOSTCFLAGS="-O2 -I/SAMA53EK/new/staging_dir/host/include -Wall -Wmissing-prototypes -Wstrict-prototypes" CROSS_COMPILE="arm-openwrt-linux-uclibcgnueabi-" ARCH="arm" KBUILD_HAVE_NLS=no CONFIG_SHELL="/usr/bin/bash" V='' CC="arm-openwrt-linux-uclibcgnueabi-gcc" dtbs
+
+
 if hash nproc 2>/dev/null
 then
     alias make="make -j$((1+$(nproc)))"
@@ -23,3 +41,4 @@ mk() {
 
 export PATH=$PATH:./scripts:./scripts/flashing
 
+alias linux-rebuild="make target/linux/{clean,compile,install} V=s"
