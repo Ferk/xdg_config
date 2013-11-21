0ddc82445c5ce08f20b5df19b601d8dac1c36432
Fernando C.V <ferkiwi@gmail.com>
2013-11-21 18:30:11 +0100

    Include svn status check for the override_cd.sh

--- sh/override_cd.sh
+++ sh/override_cd.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
 
+
+
 # Show Version Control status after cd'ing to a versioned directory
 cd() {
     [ -z "$1" ] && builtin cd && return $?
     builtin cd "$1" "$2" && {
+	## Git check
 	git rev-parse --git-dir 2>&- >&- && {
 	    git status -bs --untracked-files="no" #| column -c $COLUMNS
 	    git log -3 --pretty=format:'%an, %ar: %s' | cut -c -$COLUMNS
-	} || true
+	} || svn status -q 2>&-
+	true
     }
 }
 
