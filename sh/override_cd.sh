632e59f54126351b4780626ecfbce8300642b5eb
Fernando C.V <ferkiwi@gmail.com>
2014-01-22 11:09:48 +0100

    If the "svn status" for the cd override takes more than 1 second, it will be aborted and instead the svn status cache updated in the background so that it can show faster in the next access.

--- sh/override_cd.sh
+++ sh/override_cd.sh
@@ -7,12 +7,20 @@
 cd() {
     [ -z "$1" ] && builtin cd && return $?
     builtin cd "$1" "$2" && {
-	## Git check
-	git rev-parse --git-dir 2>&- >&- && {
+
+	## Version control check
+	if git rev-parse --git-dir 2>&- >&-
+	then
 	    git status -bs --untracked-files="no" #| column -c $COLUMNS
 	    git log -3 --pretty=format:'%an, %ar: %s' | cut -c -$COLUMNS
-	} || svn status -q 2>&-
-	true
+	else
+	    # Show svn status, but if it takes more than 1 second only cache it on background
+	    if ! timeout 1s svn status --non-interactive -q 2>&-
+	    then
+		svn status -q >&- &
+	    fi
+	fi
+
     }
 }
 
