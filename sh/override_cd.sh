354014cc6ab2d33de0bc61e81653ab1eef0dd683
Fernando C.V <ferkiwi@gmail.com>
2014-02-28 14:01:53 +0100

    Reverted last commit, will use instead another (shorter) timeout for the svn info particular case

--- sh/override_cd.sh
+++ sh/override_cd.sh
@@ -3,21 +3,22 @@
 # Show Version Control status after cd'ing to a versioned directory
 cd() {
     [ -z "$1" ] && builtin cd && return $?
-    builtin cd "$1" "$2" && timeout 1s sh <<EOF
-	## Version control check
-	if git rev-parse --git-dir >/dev/null 2>&1
-	then
-		git status -bs --untracked-files="no" #| column -c $COLUMNS
-		git log -3 --pretty=format:'%an, %ar: %s' | cut -c -$COLUMNS
+    builtin cd "$1" "$2" && {
 		
-	elif svn info >/dev/null 2>&1
-	then
-		# Show svn status, but if it takes more than 1 second only cache it on background
-		if ! svn status --non-interactive -q 2>/dev/null
+		## Version control check
+		if git rev-parse --git-dir >/dev/null 2>&1
 		then
-			svn status -q >/dev/null &
+			git status -bs --untracked-files="no" #| column -c $COLUMNS
+			git log -3 --pretty=format:'%an, %ar: %s' | cut -c -$COLUMNS
+			
+		elif timeout 0.5s svn info >/dev/null 2>&1
+		then
+			# Show svn status, but if it takes more than 1 second only cache it on background
+			if ! timeout 1s svn status --non-interactive -q 2>/dev/null
+			then
+				svn status -q >/dev/null &
+			fi
 		fi
-	fi
-EOF
+    }
 }
 
