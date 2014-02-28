98b8915ff1e65943a469fe9a9331126fcc8445c4
Fernando C.V <ferkiwi@gmail.com>
2014-02-28 13:42:28 +0100

    cd voerride timeout will apply to the whole shell command, this way we avoid problems when also checking svn info (in case of svn upgrade necesary it takes long) or other issues.

--- sh/override_cd.sh
+++ sh/override_cd.sh
@@ -1,28 +1,23 @@
 #!/bin/sh
 
-
-
-
 # Show Version Control status after cd'ing to a versioned directory
 cd() {
     [ -z "$1" ] && builtin cd && return $?
-    builtin cd "$1" "$2" && {
-
+    builtin cd "$1" "$2" && timeout 1s sh <<EOF
 	## Version control check
-	if git rev-parse --git-dir 2>&- >&-
+	if git rev-parse --git-dir >/dev/null 2>&1
 	then
-	    git status -bs --untracked-files="no" #| column -c $COLUMNS
-	    git log -3 --pretty=format:'%an, %ar: %s' | cut -c -$COLUMNS
-
-	elif svn info 2>$- >$-
+		git status -bs --untracked-files="no" #| column -c $COLUMNS
+		git log -3 --pretty=format:'%an, %ar: %s' | cut -c -$COLUMNS
+		
+	elif svn info >/dev/null 2>&1
 	then
-	    # Show svn status, but if it takes more than 1 second only cache it on background
-	    if ! timeout 1s svn status --non-interactive -q 2>&-
-	    then
-		svn status -q >&- &
-	    fi
+		# Show svn status, but if it takes more than 1 second only cache it on background
+		if ! svn status --non-interactive -q 2>/dev/null
+		then
+			svn status -q >/dev/null &
+		fi
 	fi
-
-    }
+EOF
 }
 
