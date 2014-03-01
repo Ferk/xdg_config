e8f64deb1ccb84d4c4498c3279ff2a14eff3dbb9
Fernando C.V <ferkiwi@gmail.com>
2014-03-01 02:07:25 +0100

    Added ag as an alias to ack or a grep function, whatever is available, when ag is not present itself.

---
+++ sh/ag-ack-or-grep.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Check if 'ag' is available
+# If not use ack or a custom grep function
+
+if hash ag 2>&-
+then
+    unalias ag
+    
+elif hash ack 2>&-
+then
+    alias ag=ack
+    
+else
+    ag() {
+	[ "$1" ] || { echo 'usage: ag PATTERN [FILES OR DIRECTORIES]'; return; }
+	
+	local pattern="$1"
+	shift
+	grep "${@:-.}"  --color=always --exclude=.[^.]* --exclude=node_modules -nHre "$pattern" | less -R -F -X
+    }
+fi
+
