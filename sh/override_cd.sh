e0363f63bf694e74e2849062d52587bfe99e6407
Fernando C.V <ferkiwi@gmail.com>
2014-03-08 00:00:32 +0100

    Added z.sh (thanks rupa) with small changes and integrated in the cd override

--- sh/override_cd.sh
+++ sh/override_cd.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
-# Show Version Control status after cd'ing to a versioned directory
+# Custom 'cd' override for additional info and functions
 cd() {
-    [ -z "$1" ] && builtin cd && return $?
-    builtin cd "$1" "$2" && {
-		
-		## Version control check
-		if git rev-parse --git-dir >/dev/null 2>&1
-		then
-			git status -bs --untracked-files="no" #| column -c $COLUMNS
-			git log -3 --pretty=format:'%an, %ar: %s' | cut -c -$COLUMNS
-			
-		elif timeout 0.5s svn info >/dev/null 2>&1
-		then
-			# Show svn status, but if it takes more than 1 second only cache it on background
-			if ! timeout 1s svn status --non-interactive -q 2>/dev/null
-			then
-				svn status -q >/dev/null &
-			fi
-		fi
-    }
-}
+	builtin cd "$@" || exit $?
 
+	## Show version control status
+	if git rev-parse --git-dir >/dev/null 2>&1
+	then
+		git status -bs --untracked-files="no" #| column -c $COLUMNS
+		git log -3 --pretty=format:'%an, %ar: %s' | cut -c -$COLUMNS
+
+	elif timeout 0.5s svn info >/dev/null 2>&1
+	then
+		# Show svn status, but if it takes more than 1 second only cache it on background
+		if ! timeout 1s svn status --non-interactive -q 2>/dev/null
+		then
+			svn status -q >/dev/null &
+		fi
+	fi
+
+	# Load the directory into the 'z' history (see z.sh)
+	if hash _z >/dev/null
+	then
+		_z --add "$(pwd $_Z_RESOLVE_SYMLINKS 2>/dev/null)" 2>/dev/null
+	fi
+}
