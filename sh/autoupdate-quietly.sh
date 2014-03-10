ea8cc48f593f799d4415b62cd6ffb946eb0381bd
Fernando C.V <ferkiwi@gmail.com>
2014-03-10 11:23:02 +0100

    Fix when the home directory has spaces (specially Widnows systems)

--- sh/autoupdate-quietly.sh
+++ sh/autoupdate-quietly.sh
@@ -1,7 +1,7 @@
 # runs a git pull of the .config repo once a day
 ({
-    cd $HOME/.config/
-    timelimit=$(date +%m%d)0000
+    cd "$HOME/.config/"
+    timelimit="$(date +%m%d)0000"
     touch -t $timelimit /tmp/timemarker
     if [ .git/FETCH_HEAD -ot /tmp/timemarker ]
     then
