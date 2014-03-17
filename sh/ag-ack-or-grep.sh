b0c9ba2bb79f5a5a7fefa479966e8f0c70604a13
Fernando C.V <ferkiwi@gmail.com>
2014-03-17 12:11:49 +0100

    don't show error on unalias if no alias existed before

--- sh/ag-ack-or-grep.sh
+++ sh/ag-ack-or-grep.sh
@@ -5,19 +5,19 @@
 
 if hash ag 2>&-
 then
-    unalias ag
-    
+	unalias ag 2>/dev/null
+	
 elif hash ack 2>&-
 then
-    alias ag=ack
-    
-else
-    ag() {
-	[ "$1" ] || { echo 'usage: ag PATTERN [FILES OR DIRECTORIES]'; return; }
+	alias ag=ack
 	
-	local pattern="$1"
-	shift
-	grep "${@:-.}"  --color=always --exclude=.[^.]* --exclude=node_modules -nHre "$pattern" | less -R -F -X
-    }
+else
+	ag() {
+		[ "$1" ] || { echo 'usage: ag PATTERN [FILES OR DIRECTORIES]'; return; }
+		
+		local pattern="$1"
+		shift
+		grep "${@:-.}"	--color=always --exclude=.[^.]* --exclude=node_modules -nHre "$pattern" | less -R -F -X
+	}
 fi
 
