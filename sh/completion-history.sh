e8aba0f0f22893313553d88079c22ef022d1dda6
Fernando Carmona Varo <ferkiwi@gmail.com>
2014-05-08 22:01:58 +0200

    Instead of adding completions to every not-loaded file, replace the default completion function from bash_completion

--- sh/completion-history.sh
+++ sh/completion-history.sh
@@ -11,22 +11,15 @@ complete -F _root_command sudo
 complete -F _filedir_xspec e
 complete -F _filedir_xspec mpv
 
-# Exclude some commands that load their own completion
-# at runtime
-exclude_completion_history="git"
-
 # Autocomplete using previous history
 # The COMP_LINE variable holds the currently entered input
 # COMPREPLY array will store the remaining of the matching lines, 
 # this will be done per word (no partial words will be shown).
 _history() {
     local esc_line
-    _init_completion || return
-
     esc_line=$(echo "${COMP_LINE:=$1}" | sed 's_\([][\.$\-\\|]\)_\\\1_g')
-    
+
     COMPREPLY=($(history | sed -n "s|^[0-9: \-]* ${esc_line}|${esc_line##* }|p"))
-    
     if [[ ! $COMPREPLY ]]
     then
 	# When no completion was found, use filedir on last word
@@ -36,16 +29,18 @@ _history() {
 complete -F _history \
     browser chromium firefox t mount.iso pomodoro wicd-cli xmacroplay
 
-# Apply _history completion for every suitable command in history
-# that doesn't have a completion facility already.
-for cmd in $(cat "$HISTFILE" | sed -n "s/\(\w*\).*/\1/p" | sort | uniq)
-do
-    if ! hash $cmd 2>&- || [[ $exclude_completion_history =~ $cmd ]]
-    then continue
-    fi
-    complete | grep -q " $cmd\$" || complete -F _history "$cmd"
-done
 
-# remove the variable after use
-unset completion_history_exclude
+# The "_minimal" function is used in _completion_loader
+# as the default completion function when no better completion
+# could be found.
+#
+# By default it ends up using "_filedir", let's make it use
+# our "_history" function instead
+_minimal()
+{
+    local cur prev words cword split
+    _init_completion -s || return
+    $split && return
+    _history $@
+}
 
