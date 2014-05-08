4f4165cdd7eb44eb50e48dd0903a9a3b9c9a9aa6
Fernando Carmona Varo <ferkiwi@gmail.com>
2014-05-09 00:00:58 +0200

    Added COMP_USE_FALLBACK array for including commands with _history completion as fallback.

--- sh/completion-history.sh
+++ sh/completion-history.sh
@@ -16,8 +16,13 @@ complete -F _filedir_xspec mpv
 # COMPREPLY array will store the remaining of the matching lines, 
 # this will be done per word (no partial words will be shown).
 _history() {
-    local esc_line
-    esc_line=$(echo "${COMP_LINE:=$1}" | sed 's_\([][\.$\-\\|]\)_\\\1_g')
+    # Attempt first to use alternative completion when provided
+    if [[ $COMP_HISTORY_PRECOMP ]] && eval "$COMP_HISTORY_PRECOMP"
+    then
+	[[ $COMPREPLY ]] && return
+    fi
+
+    local esc_line=$(echo "${COMP_LINE:=$1}" | sed 's_\([][\.$\-\\|]\)_\\\1_g')
 
     COMPREPLY=($(history | sed -n "s|^[0-9: \-]* ${esc_line}|${esc_line##* }|p"))
     if [[ ! $COMPREPLY ]]
@@ -26,12 +31,9 @@ _history() {
 	_filedir_xspec $@
     fi
 }
-complete -F _history \
-    browser chromium firefox t mount.iso pomodoro wicd-cli xmacroplay
-
 
 # The "_minimal" function is used in _completion_loader
-# as the default completion function when no better completion
+# as the default completion function when no other function
 # could be found.
 #
 # By default it ends up using "_filedir", let's make it use
@@ -43,4 +45,46 @@ _minimal()
     $split && return
     _history $@
 }
+
+
+# There are also some commands, like 'make', 'wget' programs that use
+# urls, etc; where it might be useful to keep the proper completion
+# function while using "_history" as a fallback when no completions
+# were found (to complete urls or other parameters).
+#
+# For this, add wrappers for 'complete' and '_completion_loader'.
+# The bash array COMP_USE_FALLBACK can be used to indicate such commands. 
+_complete_with_fallback()
+{
+    local compfn
+    if [[ $1 != '-F' || -z $2 || $2 == _minimal ]]
+    then 
+	builtin complete $@
+	return $?
+    else
+	compfn=$2
+	shift 2
+	eval "${compfn}_with_fb() { COMP_HISTORY_PRECOMP=\"$compfn\" _history \$@; }"
+	builtin complete -F ${compfn}_with_fb $@
+    fi
+}
+
+_completion_loader_with_fallback()
+{
+    for cmd in "${COMP_USE_FALLBACK[@]}"
+    do
+	if [[ $cmd == "${1##*/}" ]]
+	then	    
+	    alias complete=_complete_with_fallback
+	    _completion_loader $@
+	    unalias complete
+	    return 124
+	fi
+    done
+    _completion_loader $@
+} &&
+complete -D -F _completion_loader_with_fallback
+
+[[ $COMP_USE_FALLBACK ]] || 
+COMP_USE_FALLBACK=(make wget chromium firefox)
 
