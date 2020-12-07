144080e97247763584faff416f7f9d9f57d1be19
Fernando Carmona Varo <ferkiwi@gmail.com>
2020-12-07 13:57:19 +0100

    Add autocompletion for ssh

--- sh/10_completions.bash
+++ sh/10_completions.bash
@@ -87,6 +87,19 @@ _completion_loader_wrapper()
 } &&
 complete -D -F _completion_loader_wrapper
 
+_completion_ssh()
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts=$(grep '^Host' ~/.ssh/config ~/.ssh/config.d/* 2>/dev/null | grep -v '[?*]' | cut -d ' ' -f 2-)
+
+    COMPREPLY=( $(compgen -W "$opts" -- ${cur}) )
+    return 0
+}
+complete -F _completion_ssh ssh
+
 [[ $COMP_USE_FALLBACK ]] || 
 COMP_USE_FALLBACK=(make wget curl chromium firefox)
 
